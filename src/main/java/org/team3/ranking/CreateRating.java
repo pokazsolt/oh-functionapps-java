0647703c1c4d7fff764b9a9b8baf7da893d648c1
pokazsolt <poka.zsolt@egis.hu>
2021-10-20 11:25:14 +0200

    CreateRating impl.

---
+++ src/main/java/org/team3/ranking/CreateRating.java
@@ -0,0 +1,58 @@
+package org.team3.ranking;
+
+import java.time.Instant;
+import java.util.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class CreateRating {
+
+
+    //public static final String CONN_STRING = System.getenv("COSMOS_DB_CONN_STRING").toString();
+
+    /**
+     * This function listens at endpoint "/api/CreateRating". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/CreateRating
+     * 2. curl {your host}/api/CreateRating?name=HTTP%20Query
+     * @throws JsonProcessingException
+     */
+    @FunctionName("CreateRating")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            @CosmosDBOutput(name = "database",
+              databaseName = "Ratings",
+              collectionName = "RatingItems",
+              connectionStringSetting = "TBA")
+            OutputBinding<String> outputItem,
+            final ExecutionContext context) throws JsonProcessingException {
+
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String ratingItemStr = request.getBody().orElse(null);
+
+        context.getLogger().info("Received request JSON payload: " + ratingItemStr);
+
+        RatingItem ratingItem = mapper.readValue(ratingItemStr, RatingItem.class);
+
+        if (ratingItem == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please provide valid ratingItem json").build();
+        } else {
+            ratingItem.setId(UUID.randomUUID().toString());
+            ratingItem.setTimestamp(Instant.now().toString());
+
+            String ratingItemJson = mapper.writeValueAsString(ratingItem);
+            context.getLogger().info("RatingItem to be persisted: " + ratingItemJson);
+            outputItem.setValue(ratingItemJson);
+            
+
+            return request.createResponseBuilder(HttpStatus.OK).body(ratingItemJson).build();
+        }
+    }
+}
---
+++ src/main/java/org/team3/ranking/RatingItem.java
@@ -0,0 +1,60 @@
+package org.team3.ranking;
+
+public class RatingItem {
+
+    private String id;
+    private String userId;
+    private String productId;
+    private String timestamp;
+    private String locationName;
+    private Integer rating;
+    private String userNotes;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getProductId() {
+        return productId;
+    }
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public String getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+    public String getLocationName() {
+        return locationName;
+    }
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+    public Integer getRating() {
+        return rating;
+    }
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+    public String getUserNotes() {
+        return userNotes;
+    }
+    public void setUserNotes(String userNotes) {
+        this.userNotes = userNotes;
+    }
+    @Override
+    public String toString() {
+        return "RatingItem [id=" + id + ", locationName=" + locationName + ", productId=" + productId + ", rating="
+                + rating + ", timestamp=" + timestamp + ", userId=" + userId + ", userNotes=" + userNotes + "]";
+    }
+}
